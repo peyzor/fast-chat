15ccac311f04836e99bd1d9834dec9ff9ec2fe62
Peyman <hassani.peyman627@gmail.com>
2023-07-28 12:20:41 +0330

    Add websocket_endpoint to handle message passing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+import json
+
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
 app = FastAPI()
 
@@ -6,3 +8,42 @@ app = FastAPI()
 @app.get("/ping")
 def ping():
     return {"msg": "pong"}
+
+
+rooms = []
+
+
+async def broadcast_to_room(message: str, sender_room):
+    message = json.dumps({
+        'msg': message,
+        'userId': sender_room['client_id'],
+    })
+
+    for room in rooms:
+        if room == sender_room:
+            continue
+
+        await room['socket'].send_text(message)
+
+
+def remove_room(room):
+    for r in rooms:
+        if r == room:
+            rooms.remove(room)
+
+
+@app.websocket('/ws/{client_id}')
+async def websocket_endpoint(websocket: WebSocket, client_id: str):
+    room = {
+        "client_id": client_id,
+        "socket": websocket
+    }
+    try:
+        await websocket.accept()
+        rooms.append(room)
+        while True:
+            data = await websocket.receive_text()
+            await broadcast_to_room(data, room)
+
+    except WebSocketDisconnect as e:
+        remove_room(room)
